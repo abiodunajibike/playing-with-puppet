2d5962b3aade63d9c035881dd682390054ea5a66
Moruf <abiodun.ajibike1@yahoo.com>
2020-04-19 13:19:45 +0100

    Add more tweaks

--- scripts/configure_master.sh
+++ scripts/configure_master.sh
@@ -8,33 +8,41 @@
 # su - puppet
 
 # Add hosts ip address to /etc/hosts
+echo "------------- Adding host private IP address to /etc/hosts ------------------------"
 host_ip_address=$(ifconfig ens5 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
 echo "Host IP address: $host_ip_address"
 echo "$host_ip_address puppet" | sudo tee -a /etc/hosts
 
 # Enable the official Puppet Labs collection repository with these commands
+echo "------------- Enable the official Puppet Labs collection repository ------------------------"
 curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt-get update -y
 
 # Install the puppetserver package
+echo "------------- Installing puppet server ------------------------"
 sudo apt-get install puppetserver -y
 
 # Configure memory allocation
 ## By default, Puppet Server is configured to use 2 GB of RAM. 
 ## You can customize this setting based on how much free memory 
 ## the master server has and how many agent nodes it will manage.
+echo "------------- Updating memory allocation for puppet server ------------------------"
 sudo sed -i 's/JAVA_ARGS=.*/JAVA_ARGS="-Xms512m -Xmx512m -XX:MaxPermSize=256m"/' /etc/default/puppetserver
 
 
 # Open the firewall
+echo "------------- Opening port 8140 ------------------------"
 sudo ufw allow 8140
 
 # Start Puppet server
+echo "------------- Starting puppet server ------------------------"
 sudo systemctl start puppetserver
 
 # Configure it to start at boot
+echo "------------- Enable puppet server to boot on start up ------------------------"
 sudo systemctl enable puppetserver
 
 # See puppet server status
+echo "------------- Get puppet server status ------------------------"
 sudo systemctl status puppetserver
--- scripts/configure_node.sh
+++ scripts/configure_node.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Create puppet user on master and add to sudo group
-sudo adduser node1
-sudo usermod -aG sudo puppet
+# # Create puppet user on master and add to sudo group
+# sudo adduser node1
+# sudo usermod -aG sudo puppet
 
-# Switch to new user
-su - node1
+# # Switch to new user
+# su - node1
+
+# Add master ip address to /etc/hosts
+echo "------------- Adding master private IP address to /etc/hosts ------------------------"
+master_ip_address=$1
+echo "Master IP address: $master_ip_address"
+echo "$master_ip_address puppet" | sudo tee -a /etc/hosts
 
 # Enable the official Puppet Labs collection repository with these commands
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb
-sudo apt-get update
+sudo apt-get update -y
 
 # Install the puppet agent package
-sudo apt-get install puppet-agent
+sudo apt-get install puppet-agent -y
 
 # Start Puppet server
 sudo systemctl start puppet
