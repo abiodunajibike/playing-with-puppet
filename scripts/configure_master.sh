699db1e4c70523aa2fb5a51a2b687dab20fb0da5
Moruf <abiodun.ajibike1@yahoo.com>
2020-04-19 12:01:16 +0100

    Add more tweaks

--- scripts/configure_master.sh
+++ scripts/configure_master.sh
@@ -10,21 +10,21 @@ su - puppet
 # Add hosts ip address to /etc/hosts
 host_ip_address=$(ifconfig ens5 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
 echo "Host IP address: $host_ip_address"
-sudo echo "$host_ip_address puppet" >> /etc/hosts
+echo "$host_ip_address puppet" | sudo tee -a /etc/hosts
 
 # Enable the official Puppet Labs collection repository with these commands
 curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb
-sudo apt-get update
+sudo apt-get update -y
 
 # Install the puppetserver package
-sudo apt-get install puppetserver
+sudo apt-get install puppetserver -y
 
 # Configure memory allocation
 ## By default, Puppet Server is configured to use 2 GB of RAM. 
 ## You can customize this setting based on how much free memory 
 ## the master server has and how many agent nodes it will manage.
-sed -i 's/JAVA_ARGS*/JAVA_ARGS="-Xms512m -Xmx512m -XX:MaxPermSize=256m"'
+sed -i 's/JAVA_ARGS*/JAVA_ARGS="-Xms512m -Xmx512m -XX:MaxPermSize=256m"' /etc/default/puppetserver
 
 # Open the firewall
 sudo ufw allow 8140
