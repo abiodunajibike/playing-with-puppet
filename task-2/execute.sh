9ccebe46be6609809f7076b0d467f994a99ae9f6
Moruf <abiodun.ajibike1@yahoo.com>
2020-04-20 07:08:25 +0100

    Fix execute script

--- task-2/execute.sh
+++ task-2/execute.sh
@@ -6,16 +6,14 @@ cd /etc/puppetlabs/code/environments/production/modules
 echo "------- Creating directory lamp/manifests ----------"
 sudo mkdir -p lamp/manifests
 
-echo "------- Checking into directory lamp/manifests ----------"
-cd lamp/manifests
-
 echo "------- Copying file ./lamp/manifests/init.pp into current directory ----------"
-sudo cp -rv ./lamp/manifests/init.pp .
+sudo cp -rv ./lamp/manifests/init.pp /etc/puppetlabs/code/environments/production/modules/lamp/manifests
 
 echo "------- Checking into /etc/puppetlabs/code/environments/production/manifests directory ----------"
 cd /etc/puppetlabs/code/environments/production/manifests
 
 echo "------- Creating site.pp -------"
+sudo rm -rf site.pp
 sudo touch site.pp
 
 echo "-------  Adding content to site.pp ------- "
