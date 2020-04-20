c561aaeacb82ca7b6394627ac36f0d432e334154
Moruf <abiodun.ajibike1@yahoo.com>
2020-04-20 06:57:37 +0100

    Add task for using puppet module

---
+++ task-2/execute.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "------- Checking into /etc/puppetlabs/code/environments/production/modules directory ----------"
+cd /etc/puppetlabs/code/environments/production/modules
+
+echo "------- Creating directory lamp/manifests ----------"
+sudo mkdir -p lamp/manifests
+
+echo "------- Checking into directory lamp/manifests ----------"
+cd lamp/manifests
+
+echo "------- Copying file ./lamp/manifests/init.pp into current directory ----------"
+sudo cp -rv ./lamp/manifests/init.pp .
+
+echo "------- Checking into /etc/puppetlabs/code/environments/production/manifests directory ----------"
+cd /etc/puppetlabs/code/environments/production/manifests
+
+echo "------- Creating site.pp -------"
+sudo touch site.pp
+
+echo "-------  Adding content to site.pp ------- "
+echo "node default { include lamp }" | sudo tee -a site.pp
+
+echo "-------  Showing content of site.pp ------- "
+cat site.pp
